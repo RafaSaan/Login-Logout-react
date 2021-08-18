0cd72f8462797b75b5e2a4e0c918b57584168462
Rafa San <rafasaanchez@icloud.com>
2021-08-17 19:30:40 -0500

    error path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./Index.css";
 import App from "./App";
+import "./index.css";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
